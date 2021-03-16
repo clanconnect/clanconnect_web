9864742d84f86a71cead7dd4bcb59d66acaf5acf
Vinayak Sarawagi <vinayak.sarawagi@squareboat.com>
2021-03-16 22:22:41 +0530

    ADD cookier reader

--- src/services/configureAxios.js
+++ src/services/configureAxios.js
@@ -1,5 +1,8 @@
 import axios from "axios";
 import { toast } from "react-toastify";
+import Cookies from "js-cookie";
+
+axios.defaults.withCredentials = true;
 const axiosConfiguration = () =>
   axios.create({
     baseURL: process.env.REACT_APP_API_HOST,
@@ -10,6 +13,8 @@ const axiosConfiguration = () =>
 const axiosInstance = axiosConfiguration();
 
 axiosInstance.interceptors.request.use((config) => {
+  console.log("cookie ====> ", document.cookie);
+  console.log(Cookies.get());
   const sessionToken = window.location.pathname.includes("/influencer")
     ? process.env.REACT_APP_INFLUENCER_SESSION_TOKEN
     : process.env.REACT_APP_BRAND_SESSION_TOKEN;
