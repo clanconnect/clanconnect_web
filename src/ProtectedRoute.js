ed05e79b2d0ad72f969f97b02f718a2229754a78
Vinayak Sarawagi <vinayak.sarawagi@squareboat.com>
2021-08-25 13:56:13 +0530

    FIX: bug

--- src/ProtectedRoute.js
+++ src/ProtectedRoute.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { Redirect, Route } from "react-router-dom";
 import { useSelector } from "react-redux";
-import ResourceNotFound from "components/ResourceNotFound";
 
 const ProtectedRoute = ({ component: Component, user_type, ...rest }) => {
   const user = useSelector((store) => store.user.user);
@@ -9,31 +8,13 @@ const ProtectedRoute = ({ component: Component, user_type, ...rest }) => {
   return (
     <Route
       {...rest}
-      render={(props) =>
-        // (
-        //   <>
-        //     {user.user_type === user_type ? (
-        //       <Component {...props} />
-        //     ) : (
-        //       <Route
-        //         path="*"
-        //         component={() => {
-        //           window.location.href = `${process.env.REACT_APP_WEB_HOST}/error`;
-        //           return null;
-        //         }}
-        //       />
-        //     )}
-        //   </>
-        // )
-
-        {
-          if (user_type.includes(user.user_type)) {
-            return <Component {...props} {...rest} />;
-          } else {
-            return <Redirect to={{ pathname: "/*" }} />;
-          }
+      render={(props) => {
+        if (user && user_type.includes(user.user_type)) {
+          return <Component {...props} {...rest} />;
+        } else {
+          return <Redirect to={{ pathname: "/*" }} />;
         }
-      }
+      }}
     />
   );
 };
