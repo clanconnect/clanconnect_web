01e65481ac1351153cd2ae4b342741cc6a791f8a
ramesh <gramesh@clanconnect.ai>
2021-12-29 19:00:57 +0530

    fixed Ui things and added fullscreen button

--- src/containers/Influencer/index.js
+++ src/containers/Influencer/index.js
@@ -1,13 +1,15 @@
 import React, { useState, useEffect } from "react";
 import Header from "components/DemoHeader";
 import SideNav from "components/DemoSideNav";
-import { useLocation, useParams } from "react-router-dom";
+import { Link, useLocation, useParams } from "react-router-dom";
 import "./styles.scss";
 import { ACTIONS as PROJECT_ACTIONS } from "redux/creators/projects/actions";
 import { ACTIONS as CREATIVE_ACTIONS } from "redux/creators/creatives/actions";
 import { connect } from "react-redux";
 import DownLoadedFile from "components/DownLoadedFile";
 import { Tabs, Empty, Collapse, Row, Col } from "antd";
+import routeConstant from "common/routeConstants";
+import InfluencerUploadModal from "components/InfluencerUploadModal";
 
 const InfluencerPage = ({ creatives, projects, dispatch }) => {
   const [activeTab, setActiveTab] = useState("Creatives");
@@ -15,23 +17,27 @@ const InfluencerPage = ({ creatives, projects, dispatch }) => {
   const loadProjects = ({ proposalStatus }) => {
     dispatch({
       type: PROJECT_ACTIONS.BY_PROJECT_ID,
-      payload: { query: { proposalStatus: "accepted", include: "creatives" } ,id},
+      payload: {
+        query: { proposalStatus: "accepted", include: "creatives" },
+        id,
+      },
     });
 
     const handleActiveTab = (index) => {
       setActiveTab(index);
     };
-  }
+  };
   const loadCreatives = ({ status }) => {
     dispatch({
       type: CREATIVE_ACTIONS.GET_BY_ID,
-      payload: { query: { status:null, include: "project"},id },
+      payload: { query: { status: null, include: "project" }, id },
     });
   };
   function callback(key) {
     loadProjects({ proposalStatus: "accepted" });
-    loadCreatives({ status: '' });
+    loadCreatives({ status: "" });
   }
+  const backToViewAll = () => {};
   useEffect(() => {
     callback("pending");
   }, []);
@@ -40,37 +46,72 @@ const InfluencerPage = ({ creatives, projects, dispatch }) => {
       <Header />
       <div className="flex top-space-commom">
         <SideNav />
+
         <div className="content-wrapper">
+          <Link to={routeConstant.allCreativesListsBrand}>
+            <p className="cursor-pointer back-to-campaigns">
+              View all creatives for all campaigns
+            </p>
+          </Link>
           <div className="tabs-container">
             <div>
-            <div className={`brand-list brand-list-card`}>
-              <div className="brand-list-img">
-                <img src={projects.coverPictureUrl} alt="" />
-              </div>
-              <div className={`brand-content`}>
-                <div className="brand-list-content">
-                  <span className="list-title">{projects.title}</span>
+              <div className={`brand-list brand-list-card`}>
+                <div className="brand-list-img">
+                  <img src={projects.coverPictureUrl} alt="" />
                 </div>
+                <div className={`brand-content`}>
+                  <div className="brand-list-content">
+                    <span className="list-title">{projects.title}</span>
+                  </div>
+                </div>
+                {
+                  <div className="page-header-right-items">
+                    <div>
+                      {creatives.length !== 0 && (
+                        <InfluencerUploadModal
+                          btnText={"Upload"}
+                          style={`btn btn-outline-primary`}
+                          creativeUploads
+                          project={projects}
+                          creatives={creatives[0]?.creatives}
+                          disablePreviousVersionUpload={false}
+                          tabType={"campaign"}
+                        />
+                      )}
+                      {/* <button className="btn-submit btn-outline">Upload</button> */}
+                    </div>
+                  </div>
+                }
               </div>
-            </div>
-            <div className="open-container">
-              <Row className="file-influencer-row">
-                {creatives.length !== 0 ? (
-                  creatives[0]?.creatives.map((creative) => (
-                    <Col xs={24} md={8} sm={12} lg={6}  key={creative?.id} style={{position:'relative'}} className={`main-creatives-${creative?.status} project-influncer-file-container`}>
-                      <DownLoadedFile
-                        creative={creative}
-                        key={`creative-${creative?.id}`}
-                        project={projects}
-                        projectCard={{"status":true,"creative-status":creative?.status}}
-                      />
-                    </Col>
-                  ))
-                ) : (
-                  <Empty image={Empty.PRESENTED_IMAGE_SIMPLE} />
-                )}
-              </Row>
-            </div>
+              <div className="open-container">
+                <Row className="file-influencer-row">
+                  {creatives.length !== 0 ? (
+                    creatives[0]?.creatives.map((creative) => (
+                      <Col
+                        xs={24}
+                        md={8}
+                        sm={12}
+                        lg={6}
+                        key={creative?.id}
+                        style={{ position: "relative" }}
+                        className={`main-creatives-${creative?.status} project-influncer-file-container`}
+                      >
+                        <DownLoadedFile
+                          creative={creative}
+                          key={`creative-${creative?.id}`}
+                          project={projects}
+                          projectCard={{
+                            status: true,
+                            "creative-status": creative?.status,
+                          }}
+                        />
+                      </Col>
+                    ))
+                  ) : (
+                    <Empty image={Empty.PRESENTED_IMAGE_SIMPLE} />
+                  )}
+                </Row>
+              </div>
             </div>
           </div>
         </div>
